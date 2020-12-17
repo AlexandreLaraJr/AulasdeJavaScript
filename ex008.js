3b5664e9d12a2899c936301b4163f43af4ad8fde
Alexandre de Lara Castro Junior <68569165+AlexandreLaraJr@users.noreply.github.com>
2020-12-17 00:20:15 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 120
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if(vel>60){
+    console.log(`Você ultrapassou o limite de velocidade, acabou sendo multado!`)
+}
+console.log(`Utilize sempre o cinto de segurança`)
